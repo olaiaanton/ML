7114cc61c7d9f2721915eac0ca3e857e62048175
olaiaanton <113998297+olaiaanton@users.noreply.github.com>
2022-09-20 16:36:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,3 @@ import streamlit as st
 st.title("MY FIRST ML APP")
 
 my_text = st.text("Some text")
-
-my_button = st.button("Run ML computation")
-
-if my_button:
-    st.title("SOS")
-    
-hungry = st.checkbox("I am hungry")
-
-if hungry:
-    st.write("Go eat")
